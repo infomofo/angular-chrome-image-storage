956f13f767490e81610c6db7dda4df0eaf6e1535
Will Chiong <will@infomofo.com>
2014-02-21 22:02:45 -0500

    Add support for a "max-width" attribute that will resize the image before it is stored in memory.

--- angular-chrome-image-storage.js
+++ angular-chrome-image-storage.js
@@ -2,46 +2,74 @@
 
 angular.module("chrome-image-storage",[])
 	.factory('chrome-image-storage', function($q, $http){
+		function resizeImage(url, maxWidth, callback) {
+		    var sourceImage = new Image();
+
+		    sourceImage.onload = function() {
+		        // Create a canvas with the desired dimensions
+		        var canvas = document.createElement("canvas");
+		        console.log("maxWidth: " + maxWidth);
+		        var newWidth = Math.min(maxWidth, sourceImage.width);
+		        var newHeight = newWidth * sourceImage.height / sourceImage.width;
+		        canvas.width = newWidth;
+		        canvas.height = newHeight;
+		        // Scale and draw the source image to the canvas
+		        console.log("resizing from " + 
+		        	sourceImage.width + "x" + sourceImage.height + 
+		        	" to " + newWidth + "x" + newHeight);
+		        canvas.getContext("2d").drawImage(sourceImage, 0, 0, newWidth, newHeight);
+
+		        // Convert the canvas to a data URL in PNG format
+		        callback(canvas.toDataURL());
+		    }
+
+		    sourceImage.src = url;
+		}
 
 		/**
 		 * A function specifically for retrieving images.
 		 * 
 		 * The url is converted into a base64 image
 		 */ 
-		var getImage = function(url) {
+		var getImage = function(url, maxWidth) {
 			var deferred = $q.defer();
-			// console.log(url);
+
 			$http.get(url, {responseType: 'blob'}).success(function(blob) {
-  				// console.log('Fetched image via XHR: ' + blob);
+  				console.log('Fetched image via XHR: ' + blob);
   				var reader = new window.FileReader();
 				reader.readAsDataURL(blob); 
 				reader.onloadend = function() {
-	                var base64data = reader.result;                
-	                // console.log(base64data );
-
-	    			var keyValue = base64data;
-	    			deferred.resolve(keyValue);
+	                var base64data = reader.result;
+	                if (maxWidth != undefined) {
+		                resizeImage(base64data, maxWidth, function(dataUrl) {
+							deferred.resolve(dataUrl);	                	
+		                });
+	            	} else {
+	            		deferred.resolve(base64data);
+	            	}
 				}
     		});
 			return deferred.promise;
 		}
 
 		return {
-			getImage: function(url) {
-				return getImage(url);
+			getImage: function(url, maxWidth) {
+				return getImage(url, maxWidth);
 			},
-			getStoredImage: function(url) {
-				var area = chrome.storage.local; // change this to chrome.storage.sync for sync capabilities
-		
+			getStoredImage: function(url, maxWidth) {
 				var deferred = $q.defer();
+
+				var area = chrome.storage.local; // change this to chrome.storage.sync for sync capabilities
+			
 		        area.get(url, function(value) {
 		        	var keyValue = value[url];
 		        	if (keyValue == undefined || keyValue == null) {
-		        		getImage(url).then(function(data) {
+		        		getImage(url, maxWidth).then(function(data) {
 		        			keyValue = data;
 		        			// console.log("caching value for "+ key + " : " + angular.toJson(keyValue));
 		        			var saveObject = {};
 		        			saveObject[url] = keyValue;
+		        			// cachedImages[url] = keyValue;
 		        			area.set(saveObject, function() {
 		        				if (chrome.runtime.lasterror){
 						            console.error(chrome.runtime.lasterror.message);
@@ -62,12 +90,12 @@ angular.module("chrome-image-storage",[])
 	.directive("base64Img",function() {
 	    return {
 	        restrict: "E",
-	        scope: {dataSrc: '@ngUrl'},
+	        scope: {dataSrc: '@ngUrl',
+	    			maxWidth: '@maxWidth'},
 	        template: '<img ng-src="{{storedImage}}"/>',
 	        controller: ['$scope', '$timeout', 'chrome-image-storage', '$element',function($scope, $timeout, chromeImageStorage, $element){
-	        	$scope.chromeImageStorage = chromeImageStorage;
 	        	$scope.storedImage = null;
-	        	chromeImageStorage.getImage($scope.dataSrc).then(function(data) {
+	        	chromeImageStorage.getImage($scope.dataSrc, $scope.maxWidth).then(function(data) {
 	        		$scope.storedImage = data;
 	        	});
 	        }],
@@ -77,12 +105,12 @@ angular.module("chrome-image-storage",[])
 	.directive("storedImg", function(CSS_CLASSES){
 	    return {
 	        restrict: "E",
-	        scope: {dataSrc: '@ngUrl'},
+	        scope: {dataSrc: '@ngUrl',
+	    			maxWidth: '@maxWidth'},
 	        template: '<img ng-src="{{storedImage}}"/>',
 	        controller: ['$scope', '$timeout', 'chrome-image-storage', '$element',function($scope, $timeout, chromeImageStorage, $element){
-	        	$scope.chromeImageStorage = chromeImageStorage;
 	        	$scope.storedImage = null;
-	        	chromeImageStorage.getStoredImage($scope.dataSrc).then(function(data) {
+	        	chromeImageStorage.getStoredImage($scope.dataSrc, $scope.maxWidth).then(function(data) {
 	        		$scope.storedImage = data;
 	        	});
 	        }],
